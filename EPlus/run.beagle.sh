371f6cde8dd657cee85637796fa731ab53f5b2ac
Michael Wilde <wilde@mcs.anl.gov>
2013-12-23 20:45:18 -0600

    Change sites params for beagle: smaller maxnodes; rewrite sites file each time (dont append).

--- EPlus/run.beagle.sh
+++ EPlus/run.beagle.sh
@@ -22,12 +22,12 @@ use.provider.staging=true
 provider.staging.pin.swiftfiles=true
 wrapperlog.always.transfer=false
 sitedir.keep=false
-execution.retries=0
-lazy.errors=false
+execution.retries=2
+lazy.errors=true
 use.wrapper.staging=false
 END
 
-cat >>sites.xml <<END
+cat >sites.xml <<END
 <?xml version="1.0" encoding="UTF-8"?>
 <config xmlns="http://www.ci.uchicago.edu/swift/SwiftSites">
 <pool handle="beagle">
@@ -37,13 +37,13 @@ cat >>sites.xml <<END
  <profile namespace="globus" key="lowOverAllocation">100</profile>
  <profile namespace="globus" key="highOverAllocation">100</profile>
  <profile namespace="globus" key="providerAttributes">pbs.aprun;pbs.mpp;depth=24</profile>
- <profile namespace="globus" key="maxtime">3600</profile>
+ <profile namespace="globus" key="maxtime">7200</profile>
  <profile namespace="globus" key="project">CI-SES000031</profile>
  <profile namespace="globus" key="maxWalltime">00:20:00</profile>
  <profile namespace="globus" key="queue">batch</profile>
  <profile namespace="globus" key="userHomeOverride">$PWD</profile>
  <profile namespace="globus" key="slots">20</profile>
- <profile namespace="globus" key="maxnodes">600</profile>
+ <profile namespace="globus" key="maxnodes">10</profile>
  <profile namespace="globus" key="nodeGranularity">1</profile>
  <profile namespace="karajan" key="jobThrottle">180</profile>
  <profile namespace="karajan" key="initialScore">10000</profile>
